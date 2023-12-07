c95edf4466d1c24504eace734ae023ecad084389
TYQ <53554567+YongquanTang@users.noreply.github.com>
2023-12-07 17:00:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const glados = async () => {
       headers,
     }).then((r) => r.json())
     return [
-      'Checkin OK',
+      'Checkin OK:4',
       `${checkin.message}`,
       `Left Days ${Number(status.data.leftDays)}`,
     ]
